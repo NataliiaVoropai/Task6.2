4af42261e41a7d886b6822b4b2cc43c21a10e74a
Natalia Voropai <natalia.voropai@uvoteam.com>
2024-02-25 20:53:55 +0100

    Fixed arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from temperature import celsius_to_fahrenheit, fahrenheit_to_celsius
+from distance import meters_to_feet, feet_to_meters
 
 
 def convert_temperature(temperature_str, target_temp):
@@ -11,7 +12,16 @@ def convert_temperature(temperature_str, target_temp):
     return value
 
 
-def standardize(input_file, output_file, target_t):
+def convert_distance(distance_str, target_dist):
+    value, unit = float(distance_str.replace('ft', 'f')[:-1]), distance_str[-1]
+    if unit == "m" and target_dist == "ft":
+        return meters_to_feet(value)
+    elif unit == "f" and target_dist == "m":
+        return feet_to_meters(value)
+    return value
+
+
+def standardize(input_file, output_file, target_temp, target_dist):
     with open(input_file, 'r') as in_file, open(output_file, 'w', newline='') as out_file:
         reader = csv.reader(in_file)
         writer = csv.writer(out_file)
@@ -20,12 +30,16 @@ def standardize(input_file, output_file, target_t):
         writer.writerow(header)
 
         for row in reader:
-            date, temp_str = row
-            converted_temp = convert_temperature(temp_str, target_t)
-            writer.writerow([date, f'{round(converted_temp, 1)}°{target_t}'])
+            date, dist_str, temp_str = row
+            converted_dist = convert_distance(dist_str, target_dist)
+            converted_temp = convert_temperature(temp_str, target_temp)
+            writer.writerow([date, f'{round(converted_dist, 1)}{target_dist}',
+                            f'{round(converted_temp, 1)}°{target_temp}'])
 
 
-input_file = 'input_data.csv'
-output_file = 'standardized_measurements.csv'
-target_t = 'C'
-standardize(input_file, output_file, target_t)
+if __name__ == '__main__':
+    input_file = 'input_data.csv'
+    output_file = 'standardized_measurements.csv'
+    target_temp = 'C'
+    target_dist = 'm'
+    standardize(input_file, output_file, target_temp, target_dist)
